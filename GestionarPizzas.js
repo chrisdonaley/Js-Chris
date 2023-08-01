c1322ddc3bdd17664ef68c20d161d117b39ec70f
Christian <chrisdonaley7@gmail.com>
2023-08-01 10:53:53 -0300

    avanzamos con la web y el carrito

--- GestionarPizzas.js
+++ GestionarPizzas.js
@@ -55,7 +55,19 @@ class GestionarPizzas {
                 let prod = document.createElement("div");
                 prod.classList.add();
                 prod.id = "row_"+id;
-                prod.innerHTML = ``;
+                prod.innerHTML = `<div class="w-20">
+                                    <img src="./assets/img/${img}" alt="" width="150" height="150" >
+                                </div>
+
+                                <div class="p-3 d-flex flex-column w-60 h-150">
+                                <h3>${nombre}</h3>                                            
+                                <p>${descripcion.substring(0,120)}</p>
+                                </div>
+
+                                <div class="d-flex align-items-center justify-content-center flex-column w-20 h-150">
+                                <p class="precio">$${precio}</p>
+                                <a href="javascript:addCarrito(${id})" class="btn btn-primary">Agregar al carrito</a>
+                                </div>`;
 
 
                 divProductos.appendChild(prod);
@@ -66,5 +78,85 @@ class GestionarPizzas {
     }
 }
 
+buscar(valor) {
 
+    let resultado = pizzas.filter(pizza =>
+        pizzas.gusto.toLowerCase().includes(valor.toLowerCase()) ||
+        pizza.descripcion.toLowerCase().includes(valor.toLowerCase()));
+    this.cargarProductos(resultado);
+
+
+}
+
+
+addCart(item){
+
+
+    const existe = carrito.some(pizza => pizza.id === item.id) ;
+
+    if (existe){
+
+
+        const articulo = carrito.map(pizza=>{
+
+            if (pizza.id === item.id){
+
+
+                pizza.cantidad++;
+                return pizza;
+
+            }else{
+
+                return pizza ;
+
+            }
+
+
+        })
+
+        Toastify({
+
+            text : "Se actualizo la cantidad del producto",
+            duration: 2000 ,
+            gravity: "bottom"
+
+       }).showToast();
+       
+
+    }else{
+
+
+        carrito.push(item);
+
+        Toastify({
+
+            text : "Producto agregado con exito",
+            duration: 2000 ,
+            gravity: "bottom"
+
+       }).showToast();
+
+
+    }
+
+
+    this.actualizarCarrito();
+
+}
+
+actualizarCarrito(){
+
+    this.actualizarContador();
+    this.mostrarCarrito();
+
+    this.guardarCarrito();
+
+
+}
+
+guardarCarrito(){
+
+    //desarrollar
+
+}
 }
