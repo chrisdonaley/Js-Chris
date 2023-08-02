5fd8b633ae045974b30438cac604b1e559221117
Christian <chrisdonaley7@gmail.com>
2023-08-01 21:38:21 -0300

    comenzó a funcionar, seguimos realizando cambios

---
+++ assets/js/clases/menu.js
@@ -0,0 +1,14 @@
+class Producto {
+
+    constructor (id,gusto,precio,img, tamaño){
+
+        this.id = id;
+        this.gusto = gusto;
+        this.precio= precio;
+        this.img= img;
+        this.cantidad = 1;
+        this.tamaño = tamaño;
+
+    }
+
+}
---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+
+let carrito = [] ;
+let productos =new Array () ;
+
+let gestor ;
+
+
+const key_carrito = "carrito";
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    carrito = JSON.parse(localStorage.getItem(key_carrito)) || [] ;
+
+    gestor = new GestionarProductos() ;
+    gestor.iniciar();
+
+
+})
+
+
+
+document.querySelector("#buscar").addEventListener("keyup",()=>{
+
+
+   let q = document.querySelector("#buscar").value ;
+
+   if (q.length >= 2){
+
+    gestor.buscar(q);
+
+
+
+   }else{
+
+    gestor.mostrarHeader("Todos los productos en stock");
+    gestor.cargarProductos(productos);
+
+
+   }
+
+
+
+
+
+
+})
+
+function addCarrito(id){
+
+    const prod = document.querySelector("#row_"+id);
+
+    let titulo = prod.querySelector('h3').textContent;
+    //elimino el simbolo $ para poder convertir a numero
+    let precio = prod.querySelector(".precio").textContent.substring(1,prod.querySelector(".precio").textContent.length);
+    let img =  prod.querySelector("img").src;
+
+    let producto = new Producto (id,titulo,precio,img);
+
+    gestor.addCart(producto);
+
+}
+
+function eliminar(id){
+    gestor.eliminarProducto(id);
+}
+
+
