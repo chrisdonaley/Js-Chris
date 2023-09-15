957b0938ab72da3182bbd7aa2efc12ee831280f5
Christian <chrisdonaley7@gmail.com>
2023-09-15 17:04:14 -0300

    Vamos sumando cosas

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ const pizzaContent = document.getElementById("pizzasContent");
 const cart = document.getElementById("verCarrito");
 const cartaContainer = document.getElementById("cartaCont");
 
-let carrito = [];
+let carrito = []; //carrito vacio
 
 if (localStorage.getItem("carrito")) {
     carrito = JSON.parse(localStorage.getItem("carrito"));
     mostrarCarrito();
 }
 
-pizzas.forEach((p) => {
+pizzas.forEach((p) => { //creamos las cartas de cada producto
     const { id, nombre, precio, img, descripcion } = p;
     let content = document.createElement("div");
     content.className = "card";
@@ -28,7 +28,7 @@ pizzas.forEach((p) => {
 
     content.append(boton);
 
-    boton.addEventListener("click", () => {
+    boton.addEventListener("click", () => { //aca vamos agregando los productos al carrito.
         const existeP = carrito.findIndex((item) => item.id === id);
         if (existeP !== -1) {
             carrito[existeP].cantidad += 1;
@@ -49,11 +49,11 @@ pizzas.forEach((p) => {
     });
 });
 
-verCarrito.addEventListener("click", () => {
+verCarrito.addEventListener("click", () => {  //cuando presionamos para poder ver el carrito
     mostrarCarrito();
 });
 
-function mostrarCarrito() {
+function mostrarCarrito() {  // funcion que muestra el carrito
     cartaContainer.innerHTML = "";
     cartaContainer.style.display = "flex";
     const carta = document.createElement("div");
@@ -67,13 +67,13 @@ function mostrarCarrito() {
     cartaboton.innerText = "X";
     cartaboton.className = "carta-boton";
 
-    cartaboton.addEventListener("click", () => {
+    cartaboton.addEventListener("click", () => { //para cerrar el carrito
         cartaContainer.style.display = "none";
     });
 
     carta.append(cartaboton);
 
-    carrito.forEach((p) => {
+    carrito.forEach((p) => {  //dentro el carrito, creamos la parte que los agrega y muestra para restar y los datos de los productos agregados.
         const { id, nombre, precio, img, descripcion, cantidad } = p;
         let comprasContent = document.createElement("div");
         comprasContent.className = "carrito-content";
@@ -84,7 +84,7 @@ function mostrarCarrito() {
         <button class="restar" data-id="${id}">➖</button>
         `;
 
-        const botonRestar = comprasContent.querySelector(".restar");
+        const botonRestar = comprasContent.querySelector(".restar"); //boton de restar en el carrito.
         botonRestar.addEventListener("click", () => {
             const index = carrito.findIndex((item) => item.id === id);
             if (index !== -1) {
@@ -107,4 +107,37 @@ function mostrarCarrito() {
     totalCompra.className = "total-content";
     totalCompra.innerHTML = `Total de la compra: $${total}`;
     cartaContainer.append(totalCompra);
+
+    agregarBotonFinalizarCompra();
+}
+function agregarBotonFinalizarCompra() {        //agrega el boton finalizar compra, y también resetea el carrito, envia mensaje de si esta seguro que desea finalizar.
+    if (!document.getElementById("finalizarCompraButton")) {
+        const finalizarCompraButton = document.createElement("button");
+        finalizarCompraButton.id = "finalizarCompraButton";
+        finalizarCompraButton.innerText = "Finalizar Compra";
+
+        finalizarCompraButton.addEventListener("click", () => {   // Confirmación con SweetAlert
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: 'Una vez finalizada la compra, no se pueden realizar cambios.',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Sí, finalizar compra',
+                cancelButtonText: 'Cancelar',
+            }).then((result) => {
+                if (result.isConfirmed) {// Si se confirma la compra
+                    carrito = [];
+                    localStorage.removeItem("carrito");
+                    mostrarCarrito();
+                    Swal.fire(  // Muestra un mensaje de éxito con SweetAlert
+                        'Compra finalizada',
+                        '¡Gracias por tu compra!',
+                        'success'
+                    );
+                }
+            });
+        });
+
+        cartaContainer.appendChild(finalizarCompraButton);
+    }
 }
